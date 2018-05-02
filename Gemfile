ecd0f5266eea350876ed5f4ee70f3ca49516376a
DewaldSchoonderwoerd <your.dewithor@gmail.com>
2018-05-02 15:58:10 +0200

    stuff

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source "https://rubygems.org"
+
+gem 'cucumber'
+# gem 'geocoder'
+gem 'appium_lib'
+gem 'appium_console'
+gem 'pry-rails'
+# gem 'pry-debugger'
+gem 'byebug'
+gem 'touch_action'
+gem 'builder'
+gem 'flog'
+gem 'flay'
+gem 'ffi'
---
+++ features/locations.rb
@@ -0,0 +1,144 @@
+	def b_garden_location_9
+		{	
+    altitude: 75.0,
+    latitude: -25.738243,
+    longitude: 28.272997
+		}
+	end
+		
+	def b_garden_location_8
+		{	
+    altitude: 75.0,
+    latitude: -25.737653,
+    longitude: 28.273717
+		}
+	end
+	
+	def b_garden_location_6
+		{	
+    altitude: 75.0,
+    latitude: -25.738553,
+    longitude: 28.273717
+		}
+	end
+				
+	def b_garden_location_5
+		{	
+    altitude: 75.0,
+    latitude: -25.738753,
+    longitude: 28.274307
+		}
+	end
+			
+	def b_garden_location_4
+		{	
+    altitude: 75.0,
+    latitude: -25.738253,
+    longitude: 28.274707
+		}
+	end
+							
+	def b_garden_location_3
+		{	
+    altitude: 75.0,
+    latitude: -25.738433,
+    longitude: 28.274907
+		}
+	end						
+		
+	def b_garden_location_2
+		{	
+    altitude: 75.0,
+    latitude: -25.738593,
+    longitude: 28.275507
+		}
+	end
+			
+	def b_garden_location_1
+		{	
+    altitude: 75.0,
+    latitude: -25.737793,
+    longitude: 28.275807
+		}
+	end
+				
+	def b_garden_gym_1
+		{	
+    altitude: 75.0,
+    latitude: -25.738943,
+    longitude: 28.272397
+		}
+	end
+					
+	def b_garden_gym_2
+		{	
+    altitude: 75.0,
+    latitude: -25.738943,
+    longitude: 28.273197
+
+		}
+	end
+						
+	def b_garden_gym_3
+		{	
+    altitude: 75.0,
+    latitude: -25.737843,
+    longitude: 28.272897
+		}
+	end
+							
+	def b_garden_gym_4
+		{	
+    altitude: 75.0,
+    latitude: -25.737043,
+    longitude: 28.272197
+		}
+	end
+								
+	def b_garden_prime_spinner_spot
+		{	
+    altitude: 75.0,
+    latitude: -25.736843,
+    longitude: 28.272697
+		}
+	end
+									
+	def weavind_park_gym_1
+		{	
+    altitude: 75.0,
+    latitude: -25.735043,
+    longitude: 28.268597
+		}
+	end
+										
+	def persequor_gym_1
+		{	
+    altitude: 75.0,
+    latitude: -25.744542,
+    longitude: 28.272896
+		}
+	end
+	
+	def csir_location_1
+		{
+    altitude: 75.0,
+    latitude: -25.747864,
+    longitude: 28.275490
+		}
+	end
+	
+	def csir_location_home
+		{	
+    altitude: 75.0,
+    latitude: -25.746185,
+    longitude: 28.277190
+		}
+	end
+		
+	def csir_gym_1
+		{	
+    altitude: 75.0,
+    latitude: -25.743385,
+    longitude: 28.280289
+		}
+	end
